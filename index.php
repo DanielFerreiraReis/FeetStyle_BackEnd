344c6dcbed084be80f95b8b4add5c09225a818b9
Daniel Ferreira Reis <daniel.f.reis2007@gmail.com>
2025-10-22 00:28:13 -0300

    correção de bugs

--- index.php
+++ index.php
@@ -1,38 +1,21 @@
 <!-- Parte a ser modificada e entendida == CRUD completo com rotas REST-->
-
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Src\Database;
+require __DIR__ . '/src/Database.php';
+
 
 use Dotenv\Dotenv;
-use Src\SapatoController;
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+var_dump($_ENV); // 👈 Teste direto
 
-$controller = new SapatoController();
+echo $_ENV['APP_NAME']; // Loja de Sapatos
+echo getenv('DB_HOST'); // Deve mostrar "localhost"
 
-if ($uri === '/api/sapatos' && $method === 'GET') {
-    $controller->listar();
-} elseif ($uri === '/api/sapatos' && $method === 'POST') {
-    $controller->cadastrar();
-} elseif (preg_match('/\/api\/sapatos\/(\d+)/', $uri, $matches)) {
-    $id = $matches[1];
-    if ($method === 'PUT') {
-        $controller->atualizar($id);
-    } elseif ($method === 'DELETE') {
-        $controller->excluir($id);
-    } else {
-        http_response_code(405);
-        echo json_encode(["erro" => "Método não permitido"]);
-    }
-} else {
-    http_response_code(404);
-    echo json_encode(["erro" => "Rota não encontrada"]);
-}
+Database::conectar();
 ?>
